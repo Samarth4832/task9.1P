bd118b74b28f27f8b42dae4d2f2feacae2a1dc9b
unknown <dhiman.sahil2013@gmail.com>
2025-05-29 22:33:58 +1000

    l

--- src/main/java/cl_tdd/TaskManager.java
+++ src/main/java/cl_tdd/TaskManager.java
@@ -4,7 +4,7 @@ public class TaskManager {
     public static String getTaskStatus(String taskId) {
         if (taskId == null) return "Invalid Task ID";
         switch (taskId) {
-            case "TASK001": return "Not tarted";
+            case "TASK001": return "Not Started";
             case "TASK002": return "In Progress";
             case "TASK003": return "Submitted";
             case "TASK004": return "Feedback Received";
